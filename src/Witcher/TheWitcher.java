399f921e0eb545df01ff489aaf18980ef2d32d03
Alvaro <alvaroserrano0109@gmail.com>
2023-01-11 12:39:51 +0100

    añadidos atributos

--- src/Witcher/TheWitcher.java
+++ src/Witcher/TheWitcher.java
@@ -4,16 +4,20 @@
 package Witcher;
 
 /**
- * @author Juan Jose
+ * @author Álvaro
  *
  */
 public class TheWitcher {
-
+	private String name;
+	private int health;
+	private boolean hasSoul;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
 	}
 
