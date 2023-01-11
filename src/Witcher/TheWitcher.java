fcbb81f8584c1060f84a9266c6df2556ba9de15b
Alvaro <alvaroserrano0109@gmail.com>
2023-01-11 12:47:43 +0100

    añadidos getters y setters

--- src/Witcher/TheWitcher.java
+++ src/Witcher/TheWitcher.java
@@ -9,6 +9,24 @@ package Witcher;
  */
 public class TheWitcher {
 	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public boolean isHasSoul() {
+		return hasSoul;
+	}
+	public void setHasSoul(boolean hasSoul) {
+		this.hasSoul = hasSoul;
+	}
 	private int health;
 	private boolean hasSoul;
 	/**
